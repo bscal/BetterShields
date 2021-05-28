00e56832a4a6cf05edf7198fee9a6b509f027d55
Jon Fus <jonfus123@hotmail.com>
2021-05-28 17:00:01 -0400

    Basic networking to stop player from olding down useKey on offhand attacks

--- src/main/java/me/bscal/bettershields/bettershields/client/BetterShieldsClient.java
+++ src/main/java/me/bscal/bettershields/bettershields/client/BetterShieldsClient.java
@@ -1,14 +1,21 @@
 package me.bscal.bettershields.bettershields.client;
 
+import me.bscal.bettershields.bettershields.common.networking.BSNetManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 @Environment(EnvType.CLIENT) public class BetterShieldsClient implements ClientModInitializer
 {
 	@Override
 	public void onInitializeClient()
 	{
-
+		ClientPlayNetworking.registerGlobalReceiver(BSNetManager.STOP_USE_PACKET, (client, handler, buf, responseSender) ->
+		{
+			client.execute(() -> {
+				client.options.keyUse.setPressed(false);
+			});
+		});
 	}
 }
---
+++ src/main/java/me/bscal/bettershields/bettershields/common/networking/BSNetManager.java
@@ -0,0 +1,11 @@
+package me.bscal.bettershields.bettershields.common.networking;
+
+import me.bscal.bettershields.bettershields.BetterShields;
+import net.minecraft.util.Identifier;
+
+public class BSNetManager
+{
+
+	public static final Identifier STOP_USE_PACKET = new Identifier(BetterShields.MOD_ID, "stop_use");
+
+}
